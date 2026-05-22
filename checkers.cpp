5b040dc298f99acc7b4fec59ab2ee1fd3ebebc76
Voltronbear <zulfadhli0455@gmail.com>
2026-05-22 20:00:05 +0800

    test

---
+++ checkers.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // This program is part of the checkers game, it will display the board and pieces
+    char board[8][8] = {    
+        {' ', 'b', ' ', 'b', ' ', 'b', ' ', 'b'},
+        {'b', ' ', 'b', ' ', 'b', ' ', 'b', ' '},
+        {' ', 'b', ' ', 'b', ' ', 'b', ' ', 'b'},
+        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        {'r', ' ', 'r', ' ', 'r', ' ', 'r', ' '},
+        {' ', 'r', ' ', 'r', ' ', 'r', ' ', 'r'},
+        {'r', ' ', 'r', ' ', 'r', ' ', 'r', ' '}
+
+
+        
+    };
+    return 0;
+}
